2b8d94363da480f1d6906c0006384a4cb486986b
itzdev691 <amordarsis@gmail.com>
2026-02-08 01:10:59 -0600

    Added access point script

---
+++ ESP/Wi-Fi/access_point.py
@@ -0,0 +1,17 @@
+import network
+
+# 1. Create the access point interface object
+ap = network.WLAN(network.AP_IF)
+
+# 2. Activate the interface
+ap.active(True)
+
+# 3. Configure the netowrk credentials and security
+# Replace 'MyESP32' and 'mypassword' with your desired settings
+ap.config(essid='MyESP32'
+          password='mypassword'
+          authmode=network.AUTH_WPA_WPA2_PSK)
+
+
+# 4. Check the ESP's IP address (defauly is usally 192.168.4.1)
+print('AP Active. Network config:', ap.ifconfig())
